8e042536a55aa8aa9951f36f73952906cbba7c90
Michał Laga <zriviigeralt3693@gmail.com>
2018-04-28 17:05:43 +0200

    Update Euler.py

--- Euler.py
+++ Euler.py
@@ -3,6 +3,8 @@ class Stack:
         self.Stack = []
     def Push(self, s):  # Dodawanie elementów
         self.Stack.append(s)
+    def First(self):
+        return self.Stack[0]
     def Pop(self):  # Usuwanie elementu
         self.Stack.pop(len(self.Stack) - 1)
     def Size(self):  # Ilość elementów na stosie
@@ -23,12 +25,19 @@ def DFS(v):
             A[i][v]=0
             DFS(i)
     Euler.Push(v)
+def DFS2(v):
+    odwiedzone[v] = True
+    #print(v + 1)
+    for i in range(x):
+        if A[v][i] == 1 and odwiedzone[i] == False: DFS2(i)
 x,y=input('podaj liczbe wierzcholkow i krawedzi').split()
 x=int(x)
 y=int(y)
 A=[]
 Euler = Stack()
 odwiedzone=[]
+bla=[]
+spojny=0
 #krawedz=[]
 for i in range(x):
     A.append([])
@@ -42,22 +51,35 @@ for i in range(y):
     v1,v2=input('podaj wierzcholek poczatkowy i koncowy krawedzi ').split()
     #krawedz[i].append(int(v1))
     #krawedz[i].append(int(v2))
-    A[int(v1)][int(v2)]=1
-    A[int(v2)][int(v1)] = 1
+    A[int(v1)-1][int(v2)-1]=1
+    A[int(v2)-1][int(v1)-1] = 1
 print('macierz sąsiedztwa:')
 for t in A:
     print(t)
 for i in range(x):
     odwiedzone.append(False)
     #odwiedzone2.append(False)
-'''print('lista sasiedztwa: ')
-for i in krawedz:
-    print(i)'''
-print('Cykl Eulera:')
-DFS(0)
+DFS2(0)
+#print(odwiedzone)
+for i in odwiedzone:
+    if i==True:
+        spojny+=1
+if spojny==x:
+    print('graf spojny')
+    DFS(0)
+    #print(x,' ',Euler.Size(),Euler.Top(),Euler.First())
+    if Euler.Size()==y+1 and Euler.First()==0:
+        print('graf eulerowski')
+    elif Euler.Size()==y+1 and Euler.First()!=0:
+        print('graf poleulerowski')
+    else:
+        print('graf nieeulerowski')
+    '''for i in range(Euler.Size()):
+        print(Euler.Top())
+        Euler.Pop()'''
+else:
+    print('graf niespojny')
+
 #sciezka()
 
 #print(krawedz)
-for i in range(Euler.Size()):
-    print(Euler.Top())
-    Euler.Pop()
